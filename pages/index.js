fa5cb21550004f6ec1c0d6483de57b7ca257643d
Fabián Veliz <velizfabianhoracio@gmail.com>
2018-06-03 20:12:41 -0300

    Get data from coindesk api using isomorphic-unfetch

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
+import Fetch from 'isomorphic-unfetch';
 import Layout from '../components/Layout';
 
-const Index = () => (
+const Index = (props) => (
   <Layout>
     <h1>Welcome to Bitzprice!</h1>
   </Layout>
 );
+
+Index.getInitialProps = async () => {
+  const res = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
+  const data = await res.json();
+
+  return {
+    bpi: data.bpi,
+  }
+}
 
 export default Index;
