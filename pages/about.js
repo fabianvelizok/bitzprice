4c867f14dd4a2fcd12a02ae2f2515282a3bc0f91
Fabián Veliz <velizfabianhoracio@gmail.com>
2018-06-03 18:54:16 -0300

    Create about page and abstract navbar component

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Navbar from '../components/Navbar';
+
+const About = () => (
+  <div>
+    <Navbar />
+    <h1>About Bitzprice</h1>
+    <p>App to view Bitcoin prices</p>
+  </div>
+);
+
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,10 @@
-export default () => <div>Welcome to next.js!</div>
+import Navbar from '../components/Navbar';
+
+const Index = () => (
+  <div>
+    <Navbar />
+    <h1>Welcome to Bitzprice!</h1>
+  </div>
+);
+
+export default Index;
