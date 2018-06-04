b3df0d6616bbff4536c52a3a4984abc19f1b92a1
Fabián Veliz <velizfabianhoracio@gmail.com>
2018-06-04 00:58:43 -0300

    Update page description

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout';
 const About = () => (
   <Layout>
     <h1>About Bitzprice</h1>
-    <p>App to view Bitcoin prices</p>
+    <p>App to view Bitcoin price in different currencies.</p>
   </Layout>
 );
 
